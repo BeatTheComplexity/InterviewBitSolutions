b745ca4c30fbdb73f2459f32e312413ded240058
Suneet Agrawal <agrawalsuneet@gmail.com>
2016-09-10 13:57:07 +0530

    refactor code

--- src/arrays/simulationarray/SpiralOrderMatrixI.java
+++ src/arrays/simulationarray/SpiralOrderMatrixI.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SpiralOrderMatrixI {
-    
+
     /*
         Given a matrix of m * n elements (m rows, n columns), return all elements of the matrix in spiral order.
     
@@ -17,55 +17,54 @@ public class SpiralOrderMatrixI {
     
         You should return
         [1, 2, 3, 6, 9, 8, 7, 4, 5]
-    */
+     */
     public ArrayList<Integer> spiralOrder(final List<ArrayList<Integer>> a) {
-		 ArrayList<Integer> result = new ArrayList<>();
-		 
-		 if(a.size() <= 0) return result;
-		 
-		 int top = 0, bottom = a.size() -1, left = 0, right = a.get(0).size() -1;
-		 int dir = 0;
-		 
-		 while (top <= bottom && left<= right){
-		     switch(dir) {
-		         case 0:
-		             {
-		                 for (int i = left ; i <= right ; i++){
-		                     result.add(a.get(top).get(i));
-		                 }
-		                 top ++;
-		                 dir = 1;
-		             }
-		             break;
-		         case 1:
-		             {
-		                 for(int i = top; i <= bottom; i++){
-		                     result.add(a.get(i).get(right));
-		                 }
-		                 right --;
-		                 dir = 2;
-		             }
-		            break;
-		         case 2:
-		             {
-		                 for(int i = right; i >= left; i--){
-		                     result.add(a.get(bottom).get(i));
-		                 }
-		                 bottom --;
-		                 dir = 3;
-		             }
-		            break;
-		          case 3: 
-		              for(int i = bottom; i >= top; i--){
-		                     result.add(a.get(i).get(left));
-		                 }
-		                 left ++;
-		                 dir = 0;
-		          break;
-		                 
-		     }
-		 }
-		 
-		 return result;
-	}
+        ArrayList<Integer> result = new ArrayList<>();
+
+        if (a.size() <= 0) {
+            return result;
+        }
+
+        int top = 0, bottom = a.size() - 1, left = 0, right = a.get(0).size() - 1;
+        int dir = 0;
+
+        while (top <= bottom && left <= right) {
+            switch (dir) {
+                case 0: {
+                    for (int i = left; i <= right; i++) {
+                        result.add(a.get(top).get(i));
+                    }
+                    top++;
+                    dir = 1;
+                }
+                break;
+                case 1: {
+                    for (int i = top; i <= bottom; i++) {
+                        result.add(a.get(i).get(right));
+                    }
+                    right--;
+                    dir = 2;
+                }
+                break;
+                case 2: {
+                    for (int i = right; i >= left; i--) {
+                        result.add(a.get(bottom).get(i));
+                    }
+                    bottom--;
+                    dir = 3;
+                }
+                break;
+                case 3:
+                    for (int i = bottom; i >= top; i--) {
+                        result.add(a.get(i).get(left));
+                    }
+                    left++;
+                    dir = 0;
+                    break;
+
+            }
+        }
+
+        return result;
+    }
 }
--- src/arrays/simulationarray/SpiralOrderMatrixII.java
+++ src/arrays/simulationarray/SpiralOrderMatrixII.java
@@ -3,6 +3,7 @@ package arrays.simulationarray;
 import java.util.ArrayList;
 
 public class SpiralOrderMatrixII {
+
     /*
         Given an integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
 
@@ -12,11 +13,10 @@ public class SpiralOrderMatrixII {
 
         You should return the following matrix:
          [ [ 1, 2, 3 ], [ 8, 9, 4 ], [ 7, 6, 5 ] ] 
-    */
-    
-    
+     */
+
     public static ArrayList<ArrayList<Integer>> spiralMatrix2(int n) {
-       
+
         int[][] res = new int[n][n];
 
         int top = 0, bottom = n - 1, left = 0, right = n - 1;
@@ -57,24 +57,24 @@ public class SpiralOrderMatrixII {
                     for (int i = bottom; i >= top; i--) {
                         res[i][left] = step++;
                     }
-                    left ++;
+                    left++;
                     dir = 0;
                     break;
 
             }
         }
-        
+
         ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-        
+
         for (int i = 0; i < n; i++) {
             ArrayList<Integer> arr = new ArrayList<>(n);
             for (int j = 0; j < n; j++) {
                 arr.add(j, res[i][j]);
             }
-            
+
             result.add(i, arr);
         }
-        
+
         return result;
     }
 }
